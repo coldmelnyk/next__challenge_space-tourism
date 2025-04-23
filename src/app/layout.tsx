7047e2a06bf003c5084b0bb2acdb494fd1928a7a
Yevhenii Melnyk <rpm.steel@gmail.com>
2025-04-23 12:45:16 +0300

    renamed globals styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "@/components/Header/Header";
 import BackgroundLayout from "@/components/BackgroundLayout/BackgroundLayout";
 
 import styles from "./styles.module.scss";
-import "./globals.css";
+import "./globals.scss";
 
 const bellefair = Bellefair({
   subsets: ["latin"],
